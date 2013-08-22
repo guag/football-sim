2562b64ef01a9933ac26ebb1bbff658b2aee0b3b
Gary Guagliardo Jr <gguagliardo@gmail.com>
2013-08-22 15:57:47 -0400

    Fixed a couple of bugs in Default.aspx.cs.

--- FootballSim/FootballSim/Draft/Default.aspx.cs
+++ FootballSim/FootballSim/Draft/Default.aspx.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web.UI;
-using FootballSim.Models;
 using FootballSim.Models.Draft;
 using FootballSim.Models.Players;
 using Ninject;
@@ -12,17 +10,21 @@ namespace FootballSim.Draft
     public partial class Default : Page
     {
         [Inject]
-        public IDraftController DraftController { get; set; }
+        public IDraftController DraftController
+        {
+            get { return (IDraftController) Session["DraftController"]; }
+            set { Session["DraftController"] = value; }
+        }
 
         public IList<Player> Players
         {
-            get { return (IList<Player>)ViewState["Players"]; }
-            set { ViewState["Players"] = value; }
+            get { return (IList<Player>) Session["Players"]; }
+            set { Session["Players"] = value; }
         }
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
+            if (IsPostBack)
             {
                 return;
             }
@@ -30,9 +32,6 @@ namespace FootballSim.Draft
             Players = DraftController.SortPlayers(draft.Players);
         }
 
-        /// <summary>
-        /// TODO: this logic can be moved to the controller.
-        /// </summary>
         public IList<Player> GetPlayers(string sortExpr)
         {
             return DraftController.SortPlayers(Players, sortExpr);
